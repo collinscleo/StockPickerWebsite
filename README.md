0968c361b8042b687c2a481da660c19a27a66e09
BillyBobJoe1234567 <160171587+BillyBobJoe1234567@users.noreply.github.com>
2024-02-21 12:41:42 -0500

    IDK

--- README.md
+++
@@ -1,3 +0,0 @@
-//Welcome to the code for my website
-//The website has 4 pages: About, Dowloads, Main, and Pricing
-//Each page has two files Name.html and Name.css/NameMain.css
--- myjava.js
+++ myjava.js
@@ -1,1 +1,9 @@
+function PhotoChange1() {
+    document.getElementById('myImage').src='Pricing2.png'
+}
+function PhotoChange2() {
+    document.getElementById('myImage').src='Pricing.png'
+}
+function NextOne() {
 
+}

dcfb613345e9ec5a3f6e128ede5f0cb897f88a63
BillyBobJoe1234567 <160171587+BillyBobJoe1234567@users.noreply.github.com>
2024-02-22 19:44:23 -0500

    Edits

---
+++ LoginPage.js
@@ -0,0 +1,8 @@
+function Login() {
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    window.location.href = 'Main.html';
+});
+}

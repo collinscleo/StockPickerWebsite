bc8a706280dbaa37fd8ea7ef3d19258e377c3699
BillyBobJoe1234567 <160171587+BillyBobJoe1234567@users.noreply.github.com>
2024-03-25 12:59:06 -0400

    hello

--- Login.js
+++ Login.js
@@ -1,12 +1,16 @@
 function alt() {
     var name1 = document.getElementById("Username").value;
-    if (name1 === "Laurence") {
+    var pass = document.getElementById("Password").value;
+    if (name1 === "Laurence" && pass === "1579") {
         alert("Welcome, " + name1 + "!");
         window.location.href = "Main.html"
     } else if (name1 === "Cleo") {
         alert("Go away "+name1+" please talk to BZL to access this site.");
         window.location.href = "incorrect.html"
-    } else {
+    } else if (name1 === "Jackson") {
+        alert("Welcome, " + name1 + "!");
+        window.location.href = "incorrect.html"
+    }else {
         alert("Incorrect Username")
     }
 }
